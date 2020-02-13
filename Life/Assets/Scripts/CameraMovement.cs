a4c819df079a36581188464e82ae90f201cfe9d9
Tom <thomas.mathers@outlook.com>
2020-02-13 19:30:59 +1300

    camera

---
+++ Life/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target; //describes position
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // last method called
+    void LateUpdate()
+    {
+        if(transform.position != target.position) // camera position vs target position
+        {
+            Vector3 targetPosition = new Vector3(target.position.x,target.position.y,transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing); //linear interpelation moves % distance between target
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DateSim
+
+# https://www.youtube.com/watch?v=YJLcanHcJxo
